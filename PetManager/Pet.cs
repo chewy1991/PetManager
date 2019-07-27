d759166e3ea60ff658a5b5ea3c8599d68c762b33
Adrian Kaufmann <addi.kaufmann@bluewin.ch>
2019-07-27 18:06:06 +0200

    Added Equalsmethode, Hashcode

--- PetManager/Pet.cs
+++ PetManager/Pet.cs
@@ -16,8 +16,21 @@ namespace PetManager {
                 int hash = 17;
                 hash = hash * 23 + (this.Name != null ? this.Name.GetHashCode() : 0);
                 hash = hash * 23 + (this.Breed != null ? this.Breed.GetHashCode() : 0);
+                hash = hash * 23 + (this.Birthday != null ? this.Birthday.GetHashCode() : 0);
+
                 return hash;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Pet))
+            {
+                return object.Equals(obj, this);
+            }
+
+            var pet = (Pet)obj;
+            return string.Equals(this.Name, pet.Name) && string.Equals(this.Breed, pet.Breed) && string.Equals(this.Birthday, pet.Birthday);
         }
     }
 }
